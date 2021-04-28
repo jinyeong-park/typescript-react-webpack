51b012fe5058e26035434fc3e7cbebadd5fea9c0
I546967 <jenny.park@sap.com>
2021-04-27 17:00:20 -0700

    npm i -D typescript ts-loader to connect webpack with typescript, add tsconfig.json and edit webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // add script tag inside dist/index.html body section  after build command
 
 module.exports = {
+    entry: "./src/index.ts",
+    resolve: {
+        extensions: ['.js', ',ts', '.tsx'],
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/, 
+            loader: "ts-loader",
+            exclude: /node_modules/,
+        }]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
